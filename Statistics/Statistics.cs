d6de28ea909a5a932a1dcaaeb95598a1575c9266
Hithyshi K <hithyshi.k@philips.com>
2025-07-07 12:19:21 +0530

    Implement statistics calculator

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,9 +5,48 @@ namespace Statistics
 {
     public class StatsComputer
     {
+        public class Stats
+        {
+            public float average { get; set; }
+            public float max { get; set; }
+            public float min { get; set; }
+        }
+
         public Stats CalculateStatistics(List<float> numbers)
         {
-            //Implement statistics here
+            Stats stats = new Stats();
+
+            if (numbers.Count == 0)
+            {
+                stats.average = float.NaN;
+                stats.max = float.NaN;
+                stats.min = float.NaN;
+            }
+            else
+            {
+                float sum = 0;
+                float min = numbers[0];
+                float max = numbers[0];
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    sum += numbers[i];
+                    if (numbers[i] > max)
+                    {
+                        max = numbers[i];
+                    }
+                    else if (numbers[i] < min)
+                    {
+                        min = numbers[i];
+                    }
+                }
+
+                stats.average = sum / numbers.Count;
+                stats.max = max;
+                stats.min = min;
+            }
+
+            return stats;
         }
     }
 }
